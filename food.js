9c0ec6471419a05ed8920010bf2f349e47d5f743
Adam Macedo Malos <adamdomingosmm@gmail.com>
2023-04-06 15:21:56 -0300

    Finished.

--- food.js
+++ food.js
@@ -2,12 +2,19 @@ import { onSnake, expandSnake } from './snake.js'
 import { randomGridPosition } from './grid.js'
 
 let food = getRandomFoodPosition()
-const EXPANSION_RATE = 10
+let score = 0;
+const EXPANSION_RATE = 1
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE)
         food = getRandomFoodPosition()
+        score += 1
+        const highScore = localStorage.getItem('highScore') || 0;
+        if (score > highScore) {
+            localStorage.setItem('highScore', score);
+        }
+
     }
 
 }
@@ -18,12 +25,20 @@ export function draw(gameBoard) {
     foodElement.style.gridColumnStart = food.x
     foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
+    // score
+    const scoreElement = document.querySelector('.score');
+    scoreElement.innerText = `Score: ${score}`;
+    // highScore
+    const highScore = localStorage.getItem('highScore') || 0;
+    const highScoreElement = document.querySelector('.high-score');
+    highScoreElement.innerText = `High Score: ${highScore}`;
+
 }
 
 function getRandomFoodPosition() {
     let newFoodPosition
     while (newFoodPosition == null || onSnake(newFoodPosition)) {
-      newFoodPosition = randomGridPosition()
+        newFoodPosition = randomGridPosition()
     }
     return newFoodPosition
-  }
+}
--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js"
 
-export const SNAKE_SPEED = 8
+export const SNAKE_SPEED = 23
 const snakeBody = [{ x: 10, y: 11}]
 let newSegments = 0
 
